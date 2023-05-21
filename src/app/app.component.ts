dd8a6bd54cb6c8b6b9c81c354a1c9f3426580297
Ildut Le Fur <i7lefur@enib.fr>
2023-05-21 16:14:36 +0200

    Update Security/Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+import { GarageService } from './garage.service';
+
+import { OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { finalize } from "rxjs/operators";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ GarageService ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'garageApp';
+  cars: any;
+  greeting: any;
+
+  constructor(private garageService: GarageService, private httpClient: HttpClient, private router: Router) {
+    this.garageService.authenticate(undefined,undefined)
+  }
+
+  logout() {
+    this.httpClient.post('logout',{}).pipe(
+      finalize(() => {
+      this.garageService.authenticated = false;
+      this.router.navigateByUrl('/login');
+    })).subscribe();
+  }
+
+  ngOnInit(): void {
+      console.log("On init.....")
+      this.garageService.getCars().subscribe((datas) => {
+        this.cars = datas;
+        console.log(datas);
+      })
+      this.garageService.getGreeting().subscribe((datas) => {
+        this.greeting = datas;
+        console.log(datas);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient, HttpRequest, HttpHandler, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { GarageService } from './garage.service';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
+]
+
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GarageService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
